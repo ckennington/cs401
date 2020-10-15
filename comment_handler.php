4f2bbb51245e2cc1501215114515b7958b487811
Conrad Kennington <conradkennington@gmail.com>
2020-10-14 19:28:37 -0600

    session and cookies

--- comment_handler.php
+++ comment_handler.php
@@ -1,31 +1,37 @@
 <?php
-
+session_start();
 require_once 'Dao.php';
 require_once 'KLogger.php';
 $logger = new KLogger ("log.txt" , KLogger::DEBUG);
+$_SESSION['bad'] = array();
+$_SESSION['good'] = array();
 
 // validating
 if (!is_numeric($_POST['age'])) {
-  echo "Invalid age.";
   $logger->LogInfo("User entered invalid age [{$_POST['age']}]");
-  //header("Location: http://cs401/comments.php");
-  exit();
+  $_SESSION['bad'][] = "Invalid age";
 }
 
 if ($_POST['age'] < 2) {
-  echo "Sorry, you need to be at least 2 years old to comment.";
-  //header("Location: http://cs401/comments.php");
-  exit();
+  $_SESSION['bad'][] = "Sorry, you need to be at least 2 years old to comment.";
+}
+
+if (strlen($_POST['comment']) == 0) {
+  $_SESSION['bad'][] = "Please enter a comment";
 }
 
 if (strlen($_POST['comment']) > 256) {
-  echo "Comment is too long.";
-  //header("Location: http://cs401/comments.php");
+  $_SESSION['bad'][] = "Comment is too long.";
+}
+
+if (count($_SESSION['bad']) > 0) {
+  header("Location: http://cs401/comments.php");
   exit();
 }
 
 $dao = new Dao();
 $dao->addComment($_POST['comment']);
+$_SESSION['good'][] = "Thank you for posting";
 
 // redirect back to the comments page
 header("Location: http://cs401/comments.php");
--- comments.php
+++ comments.php
@@ -1,5 +1,8 @@
 <?php
+session_start();
 require_once 'table.php';
+
+setcookie('cs401_cookies', 'poop');
 ?>
 
 <html>
@@ -14,6 +17,17 @@ require_once 'table.php';
       <div>Comment: <input type="text" name="comment" id="comment"/></div>
       <input type="submit" value="Post a Comment">
     </form>
+    <?php
+    if (isset($_SESSION['good'])) {
+      foreach ($_SESSION['good'] as $message) {
+        echo "<div class='good'>{$message}</div>";
+      }
+      foreach ($_SESSION['bad'] as $message) {
+        echo "<div class='bad'>{$message}</div>";
+      }
+    }
+
+    ?>
       <?php
       renderTable("comments.txt");
       ?>
