8ab0a5c8aa30d1fad1f767c34b845a10a6cb9828
Conrad Kennington <conradkennington@crk.lan>
2022-02-23 21:11:40 -0700

    sanitziation and validation

--- comment.php
+++ comment.php
@@ -1,7 +1,15 @@
 <?php
 
+
+  session_start();
+  echo "<pre>" .  print_r($_SESSION,1) . "</pre>";
+  if (isset($_SESSION['message'])) {
+    unset($_SESSION['message']);
+  }
+  
   require_once 'Dao.php';
   $dao = new Dao();
+
 ?>
 
 <html>
@@ -17,6 +25,9 @@
     <h1>Comments</h1>
     <form name="commentForm" action="handler.php" method="POST">
       <div>
+        enter your age: <input type="text" name="age">
+      </div>
+      <div>
         Leave a comment: <input type="text" name="comment">
       </div>
       <div>
@@ -28,7 +39,7 @@
     echo "<table>";
     foreach ($comments as $comment) {
       echo "<tr>";
-      echo "<td>" . $comment["comment"] . "</td>";
+      echo "<td>" . htmlspecialchars($comment["comment"]) . "</td>";
       echo "<td>" . $comment["date_entered"] . "</td>";
       echo "<td class='delete'><a href='delete_handler.php?id={$comment['comment_id']}'>X</a></td>";
       echo "</tr>";
--- handler.php
+++ handler.php
@@ -1,7 +1,30 @@
 <?php
 
+  session_start();
+
+  $age = $_POST['age'];
+  $message = array();
+
+  if (!is_numeric($age)) {
+    $_SESSION['message'][] = "please enter a number";
+  } 
+
+  if ($age < 5) {
+    $_SESSION['message'][] = "uh, you're too young.";
+  }
+
+  if (count($_SESSION['message']) > 0) {
+    header('Location: comment.php');
+    exit;
+  }
+
   // grab data from the $_POST 
   $comment = $_POST['comment'];
+
+  if ($comment == "") {
+    $_SESSION['message'][] = "uh, please enter something.";
+  } 
+   
 
   require_once 'Dao.php';
   $dao = new Dao();
