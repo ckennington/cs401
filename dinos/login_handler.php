3cb7bfbfca0665366875139bdc6570bf8d68243c
Conrad Kennington <conrad.kennington@vacasa.com>
2023-03-02 09:11:18 -0700

    logger

--- dinos/login_handler.php
+++ dinos/login_handler.php
@@ -1,8 +1,12 @@
 <?php
 session_start();
 
+require_once 'KLogger.php';
+$logger = new KLogger ("log.txt" , KLogger::WARN);
+
 $username = $_POST['username'];
 $password = $_POST['password'];
+$logger->LogDebug("User [{$username}] attempting to log in");
 
 // check the database
 if ($username == 'conradkennington@gmail.com' && $password == 'abc123') {
@@ -10,7 +14,8 @@ if ($username == 'conradkennington@gmail.com' && $password == 'abc123') {
   header("Location: comments.php");
   exit();
 } else {
+  $logger->LogWarn("User [{$username}] invalid username or password");
   $_SESSION['message'] = 'Invalid Username or password';
-  header("Location: login.php");
+  header("Location: comments.php");
   exit();
 }
